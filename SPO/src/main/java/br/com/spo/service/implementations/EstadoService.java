96044001914b025adb741928734d41efc0b87002
Yuri Cavalcante <yuricavalcante1@gmail.com>
2014-09-07 14:30:21 -0300

    CidadeService

--- SPO/src/main/java/br/com/spo/service/implementations/EstadoService.java
+++ SPO/src/main/java/br/com/spo/service/implementations/EstadoService.java
@@ -1,23 +1,25 @@
 package br.com.spo.service.implementations;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import br.com.spo.model.beans.Estado;
+import br.com.spo.model.dao.impl.EstadoDao;
 import br.com.spo.service.interfaces.IEstadoService;
 
 public class EstadoService implements IEstadoService {
+    
+    private EstadoDao estadoDao;
 
     @Override
     public List<Estado> listarTodos() {
-        List<Estado> estados = new ArrayList<>();
-        for (int i = 1; i < 6; i++) {
-            Estado estado = new Estado();
-            estado.setId((long) i);
-            estado.setNome("Estado " + i);
-            estados.add(estado);
-        }
-        return estados;
+        return getEstadoDao().listarTodos();
     }
     
+    public EstadoDao getEstadoDao() {
+        if (estadoDao == null) {
+            estadoDao = new EstadoDao();
+        }
+        return estadoDao;
+    }
+
 }
