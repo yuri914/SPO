8ca9150400a9175915dcb38ca1a84a856281a4a6
Yuri Cavalcante <yuricavalcante1@gmail.com>
2014-09-07 15:46:49 -0300

    Comit de Rotina

--- SPO/src/main/java/br/com/spo/conversores/EstadoConverter.java
+++ SPO/src/main/java/br/com/spo/conversores/EstadoConverter.java
@@ -5,17 +5,36 @@ import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
 
+import br.com.spo.model.beans.Estado;
+import br.com.spo.model.dao.impl.EstadoDao;
+
 @FacesConverter(value = "estadoConverter")
 public class EstadoConverter implements Converter {
-    
+
+    private EstadoDao estadoDao;
+
     @Override
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        if ((value != null) && !value.equals("")) {
+            return getEstadoDao().buscarPorId(Long.valueOf(value));
+        }
         return null;
     }
-    
+
     @Override
     public String getAsString(FacesContext context, UIComponent component, Object value) {
-        return null;
+        if (value instanceof Estado) {
+            Estado estado = (Estado) value;
+            return String.valueOf(estado.getId());
+        }
+        return "";
+    }
+
+    public EstadoDao getEstadoDao() {
+        if (estadoDao == null) {
+            estadoDao = new EstadoDao();
+        }
+        return estadoDao;
     }
 
 }
